6d18d624f1305a55a5511294cbcc0d0088ccdcdd
Robert Pettersson <robert@bob-acken.com>
2018-05-16 00:40:07 +0200

    Implementing new header component on list and detail views

--- src/components/saloon-list.js
+++ src/components/saloon-list.js
@@ -4,7 +4,7 @@ import { Filter } from './filter';
 
 export const SaloonList = (props) => {
     const options = [
-        { value: 'all', label: 'All' },
+        { value: 'all', label: 'Alla' },
         { value: '0-249', label: 'Pris 0 - 249 kr', className: 'myOptionClassName' },
         { value: '250-500', label: 'Pris 250 - 500 kr' }
       ]
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,10 @@
 export const IMAGES = {
   RIGHT_ARROW: require('../images/right-arrow.svg'),
+  RIGHT_ARROW_WHITE: require('../images/right-arrow-white.svg'),
   MAP_POINTER: require('../images/map-pointer.svg'),
   CLOCK: require('../images/clock.svg'),
   PHONE: require('../images/phone.svg'),
-  WEB: require('../images/web.svg')
+  WEB: require('../images/web.svg'),
+  CONTROLS: require('../images/controls.svg'),
+  HEART: require('../images/heart.svg')
 }
